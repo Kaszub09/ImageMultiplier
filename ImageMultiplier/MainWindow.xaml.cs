498907870fc84d95487088bd154b154f67600803
kaszub09 <kaszub09@gmail.com>
2020-02-27 11:36:29 +0100

    Added support for multiple source files

--- ImageMultiplier/MainWindow.xaml.cs
+++ ImageMultiplier/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using Microsoft.Win32;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Controls.Primitives;
+using ImageMultiplierLibrary;
+
 
 namespace ImageMultiplier
 {
@@ -23,11 +25,13 @@ namespace ImageMultiplier
     /// </summary>
     public partial class MainWindow : Window
     {
-        String filepath=null;
+
+        List<String> filepaths = null;
         int width;
         int height;
         int percent;
         String tintSource = null;
+
 
         public MainWindow()
         {
@@ -39,8 +43,7 @@ namespace ImageMultiplier
 
         private async void multiplyButton_Click(object sender, RoutedEventArgs e)
         {
-
-            if (filepath == null)
+            if (filepaths == null)
             {
                 infoLabel.Content = "Choose the image.";
                 return;
@@ -50,153 +53,59 @@ namespace ImageMultiplier
                 infoLabel.Content = "Incorrect width or height value.";
                 return;
             }
-            dialogBoxButton.IsEnabled = false;
-            multiplyButton.IsEnabled = false;
-            Bitmap output = await CreateBitmap();
-
-            if (tintCheckBox.IsChecked == true)
-            {
-                if (!Int32.TryParse(percentTextBox.Text, out percent))
-                {
+            if (tintCheckBox.IsChecked == true) {
+                if (!Int32.TryParse(percentTextBox.Text, out percent) || percent < 0 || percent > 100) {
                     infoLabel.Content = "Incorrect tint percentage.";
-                    dialogBoxButton.IsEnabled = true;
-                    multiplyButton.IsEnabled = true;
                     return;
                 }
-                if (percent < 0 || percent > 100)
-                {
-                    infoLabel.Content = "Incorrect tint percentage.";
-                    dialogBoxButton.IsEnabled = true;
-                    multiplyButton.IsEnabled = true;
-                    return;
-                }
-                bool flag = (bool) tintWithSourceRadioButton.IsChecked;
-                if (flag)
-                {
-                    output = await Tint(new Bitmap(filepath), output, percent);
-                }
-                else
-                {
-                    if (tintSource == null)
-                    {
+                if (!(bool)tintWithSourceRadioButton.IsChecked) {
+                      if (tintSource == null) {
                         infoLabel.Content = "Choose tint image.";
                         return;
-                    }
-                    output = await Tint(new Bitmap(tintSource), output, percent);
+                      }
                 }
-
-                
             }
+            RelevantButtonVisible(false);
 
-
-            output.Save(filepath.Remove(filepath.Length- System.IO.Path.GetExtension(filepath).Length-1) + " "+width+"x"+height+".png",ImageFormat.Png);
-            output.Dispose();
-            infoLabel.Content = "Finished";
-            dialogBoxButton.IsEnabled = true;
-            multiplyButton.IsEnabled = true;
-        }
-
-        private async Task<Bitmap> CreateBitmap()
-        {
-            var progressStart = new Progress<int>(value => infoLabel.Content = "Processing image...");
-            var progress = new Progress<int>(value => infoLabel.Content = "Multiplying " + value.ToString() + " %");
-            Bitmap source=null;
-            Bitmap output=null;
-            await Task.Run(() =>
-            {
-                ((IProgress<int>)progressStart).Report(1);
-                source = new Bitmap(filepath);
-                int scaleX= source.Width * width, scaleY = source.Height * height;
-                while (scaleX > 16385 || scaleY > 16385)
-                {
-                    scaleY /= 2; scaleX /= 2;
-                }
-                source = new Bitmap(source, new System.Drawing.Size(scaleX / width, scaleY / height));
-                output = new Bitmap(source.Width * width, source.Height * height);
-
-                unsafe
-                {
-                    BitmapData sourceBmpData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-                    int* sourcePtr = (int*)sourceBmpData.Scan0;
-
-                    BitmapData outputBmpData = output.LockBits(new Rectangle(0, 0, output.Width, output.Height), ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-                    int* outputPtr = (int*)outputBmpData.Scan0;
-
-                    for (int j = 0; j < height; j++)
-                    {
-                        for (int y = 0; y < source.Height; y++)
-                        {
-                            for (int i = 0; i < width; i++)
-                            {
-                                for (int x = 0; x < source.Width; x++)
-                                {
-                                    outputPtr[0] = sourcePtr[0];
-                                    outputPtr++;
-                                    sourcePtr++;
-                                }
-                                sourcePtr -= source.Width;
-                            }
-                            sourcePtr += source.Width;
-                            ((IProgress<int>)progress).Report((y + 1 + j * source.Height) * 100 / output.Height);
-
-                        }
-                        sourcePtr = (int*)sourceBmpData.Scan0;
+            foreach (string file in filepaths) {
+                infoLabel.Content = "Processing file " + System.IO.Path.GetFileName(file);
+                Bitmap output = await Task.Run(() => ImageMultiplierClass.Multiply(new Bitmap(file), width, height));
+                RelevantButtonVisible(true);
+                if (tintCheckBox.IsChecked == true) {
+                    if ((bool)tintWithSourceRadioButton.IsChecked) {
+                        output = await Task.Run(() => ImageMultiplierClass.Tint(output, new Bitmap(file), ((float)percent) / 100));
                     }
-                    source.UnlockBits(sourceBmpData);
-                    output.UnlockBits(outputBmpData);
-                }
-            });
-
-            source.Dispose();
-            return output;
-        }
-
-        private async Task<Bitmap> Tint(Bitmap input,Bitmap output, int percent)
-        {
-            var progress = new Progress<int>(value => infoLabel.Content = "Tinting " + value.ToString() + " %");
-            input = new Bitmap(input, new System.Drawing.Size(output.Width, output.Height));
-            await Task.Run(() =>
-            {
-                unsafe
-                {
-                BitmapData sourceBmpData = input.LockBits(new Rectangle(0, 0, input.Width, input.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-                byte* sourcePtr = (byte*)sourceBmpData.Scan0;
-
-                BitmapData outputBmpData = output.LockBits(new Rectangle(0, 0, output.Width, output.Height), ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-                byte* outputPtr = (byte*)outputBmpData.Scan0;
-
-                    for (int y = 0; y < input.Height; y++)
-                    {
-                        for (int x = 0; x < input.Width * 4; x++)
-                        {
-                            outputPtr[0] = (byte)(((100 - percent) * outputPtr[0] + percent * sourcePtr[0]) / 100);
-                            outputPtr++;
-                            sourcePtr++;
-
-                        }
-                            ((IProgress<int>)progress).Report(y * 100 / input.Height);
+                    else {
+                        output = await Task.Run(() => ImageMultiplierClass.Tint(output, new Bitmap(tintSource), ((float)percent) / 100));
                     }
-                input.UnlockBits(sourceBmpData);
-                output.UnlockBits(outputBmpData);
                 }
-            });
-            input.Dispose();
-            return output;
+                output.Save(file.Remove(file.Length - System.IO.Path.GetExtension(file).Length - 1) + " " + width + "x" + height + ".png", ImageFormat.Png);
+            }
+            infoLabel.Content = "Finished.";
+            RelevantButtonVisible(true);
         }
 
-        
+        private void RelevantButtonVisible(bool visible)
+        {
+            dialogBoxButton.IsEnabled = visible;
+            multiplyButton.IsEnabled = visible;
+        }
+
 
         private void dialogBoxButton_Click(object sender, RoutedEventArgs e)
         {          
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                Multiselect = false,
+                Multiselect = true,
                 Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"
             };
             if (openFileDialog.ShowDialog() == true)
             {
-                filepath = openFileDialog.FileName;
-                filenameLabel.Content = filepath;
+                filenameLabel.Content = "";
+                filepaths = new List<string>(openFileDialog.FileNames);
+                foreach(string file in filepaths) {
+                    filenameLabel.Content = filenameLabel.Content + System.IO.Path.GetFileName(file) + ", ";
+                }            
             }             
         }
 
